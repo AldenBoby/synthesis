ae3a6943d416cde0684b3c1dbadf6218d76211d7
AldenBoby <boby.alden128@gmail.com>
2020-03-06 00:13:21 +0200

    completed up to isLeap -toTime not functional

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -13,26 +13,43 @@ let zollo a =
       |true -> -a
       |false -> a*2
 
-let min _ _ =
-    failwith "Not implemented"
+let min a b =
+    match a>b with
+      |true -> b
+      |false -> a
 
-let max _ _ =
-    failwith "Not implemented"
+let max a b =
+    match a>b with
+      |true -> a
+      |false -> b
 
-let ofTime _ _ _ =
-    failwith "Not implemented"
+let ofTime a b c =
+    (a*60*60)+(b*60)+c
 
-let toTime _ =
-    failwith "Not implemented"
+let toTime a =
+      let hour = int(a/(60*60)) in
+          let min = int(((float(a/(60*60)))-float(hour))*60.0) in 
+              let second = int(((((float(a/(60*60)))-float(hour))*60.0)-float(min))*60.0) in 
+                   (hour,min,second) 
 
-let digits _ =
-    failwith "Not implemented"
+let digits a =
+    let string = a.ToString() in
+          match string.[0] with
+              | '-' -> string.Length-1 // gets rid of negative sign
+              | _ -> string.Length
+             
 
-let minmax _ =
-    failwith "Not implemented"
+let minmax (a,b,c,d) =
+    let low = min (min a b) (min c d) in 
+            let high = max (max a b) (max c d) in 
+                 (low,high)
 
-let isLeap _ =
-    failwith "Not implemented"
+let isLeap a =
+    match a < 1582 with
+        |true -> failwith "Year cannot be less than 1582"
+        |false -> match a%4=0 && a%100 <> 0 || a%4 =0 && a%100 = 0 && a%400 = 0  with
+                    | true -> true
+                    | false -> false
 
 let month _ =
     failwith "Not implemented"
