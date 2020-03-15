032953b1c4b5a8c84dc91fbb1fe88f10b7974603
AldenBoby <boby.alden128@gmail.com>
2020-03-16 01:37:38 +0200

    finished all functions revised date and digit functions

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -29,17 +29,23 @@ let ofTime a b c =
 let toTime a =
     match a>0 with
       | true -> let hour = a/3600 in
-                        let min = System.Convert.ToInt32(((float(a)/3600.0)-float(hour))*60.0) in 
-                            let second = System.Convert.ToInt32(((((float(a)/3600.0)-float(hour))*60.0)-float(min))*60.0) in 
+                        let min = a%3600/60 in 
+                            let second = (a%3600)%60 in 
                                 (hour,min,second)
       | false -> (0,0,0)
        
 
 let digits a =
-    let string = a.ToString() in
+    (*let string = a.ToString() in
           match string.[0] with
               | '-' -> string.Length-1 // gets rid of negative sign
-              | _ -> string.Length
+              | _ -> string.Length*)
+
+     //Apadted from F# Digit Count by ShaunLuttin
+     match a = 0 with |true -> 1 |false -> match a <0 with
+                                           |true -> let a = -a
+                                                    int (log10 ((float)a)) + 1
+                                           |false -> int (log10 ((float)a)) + 1
              
 
 let minmax (a,b,c,d) =
@@ -55,21 +61,19 @@ let isLeap a =
                     | false -> false
 
 let month a =
-    match (a>=1 && a<=12,a) with
-        | (true,1) ->("January", 31)
-        | (true,2) -> ("February", 28)
-        | (true,3) -> ("March", 31)
-        | (true,4) -> ("April", 30)
-        | (true,5) -> ("May", 31)
-        | (true,6) -> ("June", 30)
-        | (true,7) -> ("July", 31)
-        | (true,8) -> ("August", 31)
-        | (true,9) -> ("September", 30)
-        | (true,10) -> ("October", 31)
-        | (true,11) -> ("November", 30)
-        | (true,12) -> ("December", 31)
-        | (false,_) -> failwith "Not a valid month"
-                    
+    match (a>=1 && a<=12,a) with | (false,_) -> failwith "Not a valid month"
+                                 | (true,1) ->("January", 31)
+                                 | (true,2) -> ("February", 28)
+                                 | (true,3) -> ("March", 31)
+                                 | (true,4) -> ("April", 30)
+                                 | (true,5) -> ("May", 31)
+                                 | (true,6) -> ("June", 30)
+                                 | (true,7) -> ("July", 31)
+                                 | (true,8) -> ("August", 31)
+                                 | (true,9) -> ("September", 30)
+                                 | (true,10) -> ("October", 31)
+                                 | (true,11) -> ("November", 30)
+                                 | (true,12) -> ("December", 31)          
 
 let toBinary a =
     match a >= 0 with
@@ -81,44 +85,54 @@ let toBinary a =
                 bin a
        | false -> failwith "Cannot use negative numbers"
 
-let bizFuzz a =
-      let divide a = a
-      divide a
-       
-    
-
+let bizFuzz n =
+     let rec multiplier div3 div5 div35 count=
+        match count<=n with
+           | false -> (div3,div5,div35)
+           | true -> match count%3 = 0 && count%5 = 0 with
+                        |true -> multiplier (div3 + 1) (div5 + 1) (div35 + 1) (count + 1)
+                        |false -> match count%3 = 0 with
+                                     |true -> multiplier (div3 + 1) div5 div35 (count + 1)
+                                     |false -> match count%5 = 0 with
+                                                  |true -> multiplier div3 (div5 + 1) div35 (count + 1)
+                                                  |false ->multiplier div3 div5 div35 (count + 1)
+     multiplier 0 0 0 1
+                      
+                      
+                      
+           
 let monthDay d y =
-    let month d leap = match (d>0 && d<=31) with
-                            | true -> "January"
-                            | false -> match (d>31 && d<=59+leap) with
-                                          | true -> "February"
-                                          | false -> match (d>59+leap && d<=90+leap) with 
-                                                        | true -> "March"
-                                                        | false -> match (d>90+leap && d<=120+leap) with
-                                                                     |true -> "April"
-                                                                     |false -> match (d>120+leap && d<=151+leap) with
-                                                                                 | true -> "May"
-                                                                                 | false -> match (d>151+leap && d<=181+leap) with
-                                                                                              | true -> "June"
-                                                                                              | false -> match (d>181+leap && d<=212+leap) with
-                                                                                                           | true -> "July"
-                                                                                                           | false -> match (d>212+leap && d<=243+leap) with
-                                                                                                                        | true -> "August"
-                                                                                                                        | false -> match (d>243+leap && d<=273) with
-                                                                                                                                     | true -> "September"
-                                                                                                                                     | false -> match (d>273+leap && d<=304+leap) with
-                                                                                                                                                  | true -> "October"
-                                                                                                                                                  | false -> match (d>304+leap && d<=334+leap) with
-                                                                                                                                                               | true ->"November"
-                                                                                                                                                               | false -> match (d>334+leap && d<=365+leap) with
-                                                                                                                                                                             | true -> "December"
-                                                                                                                                                                             | false -> failwith "wot"
-    match d>0 && d<=365 && y <1582 with 
-        | true -> failwith "out of bounds"
-        | false -> match isLeap y with
-                    | true -> month d 1           
-                    | false -> month d 0
-    
+    let getmonth (a,_) = a
+    let getday (_,b) = b
+    let rec findmon day start fin mon count leap =
+            match (day>=start+leap) && (day<=fin+leap) with
+                    |true -> mon
+                    |false ->let start=fin
+                             match isLeap y with 
+                             |true -> findmon day start (fin+(getday (month (count+1)))) (getmonth (month (count+1))) (count+1) 1
+                             |false -> findmon day start (fin+(getday (month (count+1)))) (getmonth (month (count+1))) (count+1) 0
+                             
+    match d>0 && d<=366 && y >=1582 with 
+        | false -> failwith "out of bounds"
+        | true -> findmon d 1 (getday (month 1)) (getmonth (month 1)) 1 0
 
-let coord _ =
-    failwith "Not implemented"
+let coord (x,y:float) =
+    //sqrt function from textbook
+
+    let sqrt n =
+        let rec calculate guess i = 
+            match i with
+            | 10 -> guess
+            | _ -> 
+                let g = (guess + n/guess) / 2.0
+                calculate g (i + 1)
+        match n <= 0.0 with 
+        | true -> failwith "cannot be less than zero"
+        | _ -> calculate (n/2.0) 0
+
+    
+    let distance (x1,y1) = sqrt((x1-x)**2.0 + (y1-y)**2.0)
+
+    let square (x1,y1) (width:float) (height:float) = match x >= (x1) && x<=(x1+width)  with |false ->false |true ->true
+        
+    (distance,square)
